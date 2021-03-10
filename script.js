aa4b4379fcecec18710f6e890f0898ba2740fcd1
Kalina <kali0173@stud.kea.dk>
2021-03-10 21:32:58 +0100

    working, just gotta fix the wood material thing

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 "use strict";
 
-//Global variables
+// Global variables
 let elementToPaint;
+
+// Model for the features
+const features = {
+  blackLamp: false,
+  blueLamp: false,
+  plant: false,
+  globe: false
+}
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  console.log("init");
-
   loadSVG();
 }
 
@@ -17,50 +23,107 @@ function loadSVG() {
     .then((response) => response.text())
     .then((desk) => {
       document.querySelector(".desk").innerHTML = desk;
-    });
-  addInteractivity();
+    })
+    .then(addInteractivity)
 }
 
 function addInteractivity() {
-  console.log("addInteractivity");
-  document.querySelectorAll(".interact").forEach((deskelement) => {
-    console.log(deskelement);
-
-    deskelement.addEventListener("click", clickOn);
-    deskelement.addEventListener("mouseover", mouseOver);
-    deskelement.addEventListener("mouseout", mouseOut);
+  document.querySelectorAll(".interact").forEach((deskElement) => {
+    deskElement.addEventListener("click", clickOn);
+    deskElement.addEventListener("mouseover", mouseOver);
+    deskElement.addEventListener("mouseout", mouseOut);
   });
 
-  document.querySelectorAll(".colorbox").forEach((coloroption) => {
-    coloroption.addEventListener("click", chooseColor);
+  document.querySelectorAll(".colorbox").forEach((colorOption) => {
+    colorOption.addEventListener("click", chooseColor);
   });
 
-  document.querySelectorAll(".materialbox").forEach((materialoption) => {
-    materialoption.addEventListener("click", chooseMaterial);
+  document.querySelectorAll(".materialbox").forEach((materialOption) => {
+    materialOption.addEventListener("click", chooseMaterial);
   });
+
+  document.querySelectorAll(".feature-item").forEach((featureItem) =>
+    featureItem.addEventListener("click", toggleFeatures));
 }
 
 function clickOn() {
-  console.log("clickOn");
-
   elementToPaint = this;
-  this.style.fill = "red";
+  this.style.fill = "grey";
+
+  if (this == "right-leg") {
+    console.log("o")
+    document.querySelector("li #top-desk").style.fontWeight = "bold"
+  }
 }
 
 function mouseOver() {
-  console.log("mouseOver");
-  this.style.strokeStyle = "blue";
+  this.style.stroke = "red";
   this.style.cursor = "pointer";
 }
 
 function mouseOut() {
-  this.style.strokeStyle = "none";
+  this.style.stroke = "none";
 }
 
 function chooseColor() {
   if (elementToPaint != undefined) {
-    elementToPaint.style.fill = this.getAttribute("backgroundColor");
+    elementToPaint.style.fill = this.getAttribute("fill");
   }
 }
 
-function chooseMaterial() {}
+function chooseMaterial() {
+  if (elementToPaint != undefined) {
+    elementToPaint.style.fill = "#cb997e";
+  }
+}
+
+function toggleFeatures(event) {
+  const target = event.currentTarget;
+  const feature = target.dataset.feature;
+
+  // Toggles the features
+  features[feature] = !features[feature]
+
+  if (features[feature] == true) {
+    // Adds the feature element
+    target.classList.add("chosen");
+    document.querySelector(`[data-feature="${feature}"]`).classList.remove("hide");
+
+    const featureElement = createFeatureElement(feature);
+    document.querySelector("#selected ul").appendChild(featureElement);
+
+    // FLIP
+    const start = target.getBoundingClientRect();
+    const end = featureElement.getBoundingClientRect();
+
+    const diffX = start.x -end.x;
+    const diffY = start.y - end.y;
+
+    featureElement.style.transform = `translate(${diffX}px, ${diffY}px)`; 
+    featureElement.offsetHeight;
+
+    featureElement.style.transition = "transform 1s";
+    featureElement.style.transform = "translate(0,0)";
+
+  } else {
+    // Removes the feature element
+    target.classList.remove("chosen");
+    const featureElement = document.querySelector(`#selected [data-feature="${feature}"]`);
+    featureElement.classList.add("hide");
+
+    document.querySelector(`[data-feature="${feature}"]`).classList.add("hide");
+  }
+}
+
+// Create a feature element, which is appened to the #selected ul
+function createFeatureElement(feature) {
+  const li = document.createElement("li");
+  li.dataset.feature = feature;
+
+  const img = document.createElement("img");
+  img.src = `${feature}.png`;
+
+  li.append(img);
+
+  return li;
+}
